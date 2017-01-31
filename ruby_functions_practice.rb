cf05b81489d37a2e3d95c177612b8b248b323710
Ferdinando <f.sendyka@gmail.com>
2017-01-31 15:54:17 +0000

    1st version

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,31 @@
+def return_10
+  return 10
+end
+
+def add(a, b)
+  return a + b
+end
+
+def subtract(a, b)
+  return a - b
+end
+
+def multiply(a, b)
+  return a * b
+end
+
+def divide(a, b)
+  return a/b
+end
+
+def length_of_string(a)
+  return a = a.length.to_i
+end
+
+def join_string(a, b)
+  return "a + b"
+end
+
+def add_string_as_as_number(a, b)
+  return "#{a.length.to_i} + #{b.length.to_i}"
+end
